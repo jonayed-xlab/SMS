c1eac9737852d934293e6fe5021d616349d5adf3
Jonayed <jonayed@pop-os.localdomain>
2023-01-28 18:46:22 +0600

    Create a new db executor-server microservice.

---
+++ executor-server/src/main/java/com/jontech/executorserver/ExecutorServerApplication.java
@@ -0,0 +1,13 @@
+package com.jontech.executorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutorServerApplication.class, args);
+	}
+
+}
---
+++ executor-server/src/test/java/com/jontech/executorserver/ExecutorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jontech.executorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
